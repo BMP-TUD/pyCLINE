78990cff517b47e64b962ae306f26e86d3bcee97
Bartosz <bartosz.prokop@kuleuven.be>
2025-03-06 12:25:39 +0100

    implementing sphinx building of pages and docs

--- source/conf.py
+++ source/conf.py
@@ -16,7 +16,7 @@ release = '0.1.4'
 
 extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
 
-templates_path = ['build/_templates']
+templates_path = ['_templates']
 exclude_patterns = []
 
 
@@ -24,5 +24,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
-html_static_path = ['build/_static']
+# html_theme = "sphinx_rtd_theme"
+# html_static_path = ['_static']
